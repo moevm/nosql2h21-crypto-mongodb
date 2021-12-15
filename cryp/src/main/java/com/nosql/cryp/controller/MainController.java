87514d929dccfe71117d6f22c0082e383128bf34
Regedit188 <42926970+Regedit188@users.noreply.github.com>
2021-12-15 14:55:52 +0300

    mainPage controller

--- cryp/src/main/java/com/nosql/cryp/controller/MainController.java
+++ cryp/src/main/java/com/nosql/cryp/controller/MainController.java
@@ -18,18 +18,9 @@ public class MainController {
 
         return "redirect:/currency/apiFull";
     }
-    @GetMapping("/main")
+    @GetMapping("/mainPage")
     public String showHomePage(Model model){
-        /*
-        Date date1 = new Date(453221);
-        Date date2 = new Date(143232);
-        Currency currency1 = new Currency("BTC", date1, 10.23);
-        Currency currency2 = new Currency("DG", date2, 1.15);
-        List<Currency> currencies = new ArrayList<Currency>();
-        currencies.add(currency1);
-        currencies.add(currency2);
-        model.addAttribute("currencies", currencies);
-         */
-        return "main";
+        
+        return "mainPage";
     }
 }
